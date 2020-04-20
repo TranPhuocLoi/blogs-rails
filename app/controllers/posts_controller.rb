42fca848f4c2b9a6fcec7d411234344aaed863c1
DuongTran <ttduongtran@gmail.com>
2020-04-21 00:49:15 +0700

    fix missing user_id

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,6 +10,7 @@ class PostsController < ApplicationController
   # GET /posts/1
   # GET /posts/1.json
   def show
+    @post = Post.find_by_id(params[:id])
   end
 
   # GET /posts/new
@@ -25,7 +26,7 @@ class PostsController < ApplicationController
   # POST /posts.json
   def create
     @post = Post.new(post_params)
-    # @post.users = current_user
+    @post.user = current_user
     
     respond_to do |format|
       if @post.save
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,4 @@
 class Post < ApplicationRecord
-  belongs_to:user
-  validates_presence_of:title
+  belongs_to :user
+  validates_presence_of :title
 end
